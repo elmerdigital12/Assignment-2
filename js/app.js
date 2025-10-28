3e4eedd8e5935a070e2e4f048c31f6772a482512
elmerdigital12 <elmer.wipe@gmail.com>
2025-10-28 16:59:20 +0100

    cons

--- js/app.js
+++ js/app.js
@@ -1,17 +1,18 @@
-fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
-  .then(response => {
-    if (!response.ok) {
-      throw new Error("Failed to fetch response");
-    }
-    return response.json();
-  })
-  .then(data => {
-    const sorted = data.categories.sort((a, b) =>
-      a.strCategory.localeCompare(b.strCategory)
-    );
-    const first5 = sorted.slice(0, 5);
-    console.log("First 5 categories in order:");
-    first5.forEach(item => console.log(item.strCategory));
+const letters = "abcdefghijklmnopqrstuvwxyz".split("");
+Promise.all(
+  letters.map(letter =>
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=`)
+      .then(response => response.json())
+      .then(data => data.meals || [])
+  )
+)
+  .then(allMealsArrays => {
+    const allMeals = allMealsArrays.flat();
+    const first5Names = allMeals
+      .sort((a, b) => a.strMeal.localeCompare(b.strMeal))
+      .slice(0, 5)
+      .map(meal => meal.strMeal);
+    console.log("First 5 meals in order a-z:");
+    console.log(first5Names);
   })
   .catch(error => console.error("Error:", error));
-
