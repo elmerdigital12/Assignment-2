ea9c63ab1eab972c89a2b24ece6e40a9edd69dd2
elmerdigital12 <elmer.wipe@gmail.com>
2025-10-28 17:06:24 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
 const letters = "abcdefghijklmnopqrstuvwxyz".split("");
 Promise.all(
   letters.map(letter =>
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=`)
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${letter}`)
       .then(response => response.json())
       .then(data => data.meals || [])
   )
 )
   .then(allMealsArrays => {
     const allMeals = allMealsArrays.flat();
-    const first5Names = allMeals
+    const first5Meals = allMeals
       .sort((a, b) => a.strMeal.localeCompare(b.strMeal))
       .slice(0, 5)
       .map(meal => meal.strMeal);
     console.log("First 5 meals in order a-z:");
-    console.log(first5Names);
+    console.log(first5Meals);
   })
   .catch(error => console.error("Error:", error));
